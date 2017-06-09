ffcc3ae4ffd5e360aa7f2495c6581e6003ec99ca
Brett Schules <brettschules@gmail.com>
2017-06-09 16:53:24 -0400

    Done

--- js/events.js
+++ js/events.js
@@ -2,6 +2,43 @@
 
 $(document).ready(function(){
 
-// call functions here
+
+
+
+
+frameIt()
+
+getIt()
+
+pressIt()
+
+submitIt()
 
 });
+
+function getIt() {
+  $('p').click(function(){
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').load(function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').keydown(function(event) {
+    if(event.which === 71) {
+      alert("g key was pressed down")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').submit(function (event) {
+    alert('Your form is going to be submitted now.')
+    event.preventDefault()
+  })
+}
